df6227ef7961d727b35ff1aa81c731a958558d6a
Pran-Ker <hebbarpran@gmail.com>
2023-02-03 18:38:22 +0530

    Base app

---
+++ example-list.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import RegisterForm from "./examples/register-form/RegisterForm";
+
+
+export const EXAMPLE_LIST = [
+  
+  {
+    name: "Register Form",
+    component: <RegisterForm />,
+  },
+  
+];
